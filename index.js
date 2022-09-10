e9f5ff4022254dfe7e6408d33c4131871a1ceec7
stacyvana <ssvana83@yahoo.com>
2022-09-10 13:05:36 -0400

    commit tree c73ac3c6519b1dcf4e9d8f43eae4855e432b3a16

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
+// let input = document.getElementById('input')
+
+// function addingEventListener() {
+//   input.addEventListener('click', function() {
+//     alert('I was clicked');
+//   });
+// }
+
+// function addingEventListener() {
+//   const input = document.getElementById('input');
+//     alert('I was clicked!');
+//     input.addingEventListener('click', addingEventListener);
+//   }
+
+// addingEventListener();
+// input.addingEventListener('click', addingEventListener);
+
 function addingEventListener() {
+  const input = document.getElementById('button');
+  input.addEventListener('click', function() {
+    alert('I was clicked!');
+  });
 }
+
+addingEventListener()
